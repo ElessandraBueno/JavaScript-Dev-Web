b803d32e821506e58b137b9b8f43ca3b5038a307
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-17 08:29:27 -0300

    Pontos importantes - Números

--- fundamentos/numeros.js
+++ fundamentos/numeros.js
@@ -15,3 +15,11 @@ console.log(media.toFixed(2)); //.toFixed não altera o valor da variável, apen
 console.log(media.toString()); //Altera o tipo do resultado para String
 console.log(typeof media);
 
+/*ATENÇÃO A ALGUNS DETALHES*/
+console.log(7 / 0); //infinito
+console.log("10" / 2); //considera a String como número e realiza a divisão
+console.log("3" + 2); //pelo fato de começar com uma String e o operador ser o + que é o de concatenação, ele não soma e sim concatena
+console.log('5' - 2); //considera a String como número e realiza a subtração
+console.log("Show!" * 2); //não repete a String
+console.log(0.1 + 0.7); //não retorna 0.8 opr conta da precisão do float
+console.log((10.356).toFixed(2)); 
