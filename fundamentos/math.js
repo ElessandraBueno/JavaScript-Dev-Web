70385bcf70b5fecc5dd05aee5eca9278de55bb5b
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-17 07:31:14 -0300

    Math

---
+++ fundamentos/math.js
@@ -0,0 +1,20 @@
+const raio = 5.6;
+const area = Math.PI * Math.pow(raio, 2);
+
+console.log(area);
+console.log("O tipo de Math é: "+typeof Math);
+
+const absoluto = Math.abs(-56);
+console.log("O valor absoluto é: "+absoluto);
+
+const maximo = Math.max(9,3);
+console.log("O valor máximo é: "+maximo);
+
+const minimo = Math.min(56,2);
+console.log("O valor mínimo é: "+minimo);
+
+const arred = Math.round(23.5);
+console.log("O número arredondado é: "+arred);
+
+const raizquadrada = Math.sqrt(25);
+console.log("A raiz quadrada é: "+raizquadrada);
