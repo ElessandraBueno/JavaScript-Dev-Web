68f3d2d9f3252508daa117c0911719d887ae2826
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-18 21:36:49 -0300

    básico de objetos

---
+++ fundamentos/objeto.js
@@ -0,0 +1,14 @@
+const prod1 = {};
+prod1.nome = "Celular Ultra";
+prod1.preco = 4899.90;
+
+prod1['desconto maximo'] = 0.50;
+
+console.log(prod1);
+
+const prod2 = {
+    nome: 'camisa polo',
+    preco: 78.90
+}
+
+console.log(prod2);
