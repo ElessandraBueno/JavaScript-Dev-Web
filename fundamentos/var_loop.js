81a1f020e90428e98607ba32fdd3d7863d2c7f17
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-21 19:53:47 -0300

    Loop  - variáveis

--- fundamentos/var_loop.js
+++ fundamentos/var_loop.js
@@ -3,3 +3,16 @@ for(var i = 0; i <10; i++){
 }
 //o valor de i é o valor no qual fez ele parar o laço, ou seja, 10
 console.log('i =', i)
+
+
+//VAR não tem escopo de função
+const funcs = []
+
+for(var i = 0; i < 10; i++){
+    funcs.push(function (){
+        console.log(i)
+    })
+}
+
+funcs[2]()
+funcs[8]()
