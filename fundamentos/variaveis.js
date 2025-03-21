400a481ea05f4f19f1407ea91a60310d9ea9887e
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-21 10:24:46 -0300

    Ajuste - variáveis

--- fundamentos/variaveis.js
+++ fundamentos/variaveis.js
@@ -8,9 +8,18 @@
 }
 console.log(a)
 
+var numero = 1
+{
+    var numero = 2
+    console.log('dentro =',numero)
+}
+console.log("fora = "+numero)
+
 //ESCOPO DE FUNÇÃO
 function teste(){
     var local = 123
 }
 teste()
 console.log(local)
+
+
