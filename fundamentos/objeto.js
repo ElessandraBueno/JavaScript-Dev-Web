fc3435b624804c9023c406b0b5c496bfa859389b
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-21 20:31:39 -0300

    Instância

--- fundamentos/objeto.js
+++ fundamentos/objeto.js
@@ -10,5 +10,21 @@ const prod2 = {
     nome: 'camisa polo',
     preco: 78.90
 }
-
 console.log(prod2);
+console.log('*******************************')
+
+//Instância de função
+console.log('Tipo de Object =',typeof Object)
+console.log('Tipo de um Object instanciado = '+ typeof new Object)
+console.log('          *******')
+
+const Cliente = function(){}
+console.log('O tipo da constante Cliente = '+ typeof Cliente)
+console.log('O tipo da constante instanciada = '+ typeof new Cliente)
+console.log('          *******')
+
+class Produto {}
+console.log('O tipo da classe Produto = '+ typeof Produto)
+console.log('O tipo da classe instanciada = '+ typeof new Produto)
+console.log('*******************************')
+
