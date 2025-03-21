36292dd6543625de3c9bcfedfd61af8c349b4c5e
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-21 10:47:36 -0300

    Let

--- fundamentos/let.js
+++ fundamentos/let.js
@@ -6,8 +6,16 @@ var numero = 1
 }
 console.log('fora =', numero)
 
+//Não pode ser utilizada fora do escopo de bloco
 {
     let numero1 = 2
     console.log('dentro =', numero1)
 }
 console.log('fora =', numero1)
+
+//Não pode ser utilizada fora do escopo de função
+function a(){
+    let teste = 3;
+}
+a()
+console.log(teste)
