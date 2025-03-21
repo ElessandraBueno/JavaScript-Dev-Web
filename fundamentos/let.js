c529110ba916d28d5a6e9afaf807ebe62f18017e
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-21 19:56:32 -0300

    Loop - let

--- fundamentos/let.js
+++ fundamentos/let.js
@@ -7,15 +7,28 @@ var numero = 1
 console.log('fora =', numero)
 
 //Não pode ser utilizada fora do escopo de bloco
-{
-    let numero1 = 2
-    console.log('dentro =', numero1)
-}
-console.log('fora =', numero1)
+// {
+//     let numero1 = 2
+//     console.log('dentro =', numero1)
+// }
+// console.log('fora =', numero1)
 
 //Não pode ser utilizada fora do escopo de função
-function a(){
-    let teste = 3;
+// function a(){
+//     let teste = 3;
+// }
+// a()
+// console.log(teste)
+
+
+//
+const funcs = []
+
+for(let i = 0; i < 10; i++){
+    funcs.push(function (){
+        console.log(i)
+    })
 }
-a()
-console.log(teste)
+
+funcs[2]()
+funcs[8]()
