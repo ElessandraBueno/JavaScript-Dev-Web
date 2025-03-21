ad71bdb0e55134e3e9196b6ba4723e5271b3c303
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-21 20:07:24 -0300

    Hoisting

---
+++ fundamentos/hoisting.js
@@ -0,0 +1,9 @@
+//Variaveis declaradas com VAR tem o hoisting
+console.log('a =', a)
+var a = 2
+console.log('a =',a)
+
+//Varáveis declaradas com LET não tem hoisting
+console.log('b =',b)
+let b = 3
+console.log()
