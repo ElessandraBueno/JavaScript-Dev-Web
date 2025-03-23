63edc06abbaed64711f1cfb96e62ca398f4c3fa2
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-23 20:35:41 -0300

    Destructuring com array

--- fundamentos/destructuring.js
+++ fundamentos/destructuring.js
@@ -24,3 +24,15 @@ console.log(sobrenome, maiorIdade)
 //destructuring buscando objeto dentro de objeto
 const {endereco: {logradouro, numero, cep}} = pessoa
 console.log(logradouro, numero, cep)
+
+//DESTRUCTURING COM ARRAY
+
+console.log('****************************')
+const [a] = [10]
+console.log(a)
+
+const [n1, , n3, , n5, n6 = 0] = [10, 7, 9, 8]
+console.log(n1, n3, n5, n6)
+
+const [, [, nota]] = [[, 8, 8], [9, 6, 8]]
+console.log(nota)
