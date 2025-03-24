e75553f2be675d9fd82968adaa5f3f7e5f49e332
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-24 08:04:31 -0300

    Destructuring com função

--- fundamentos/destructuring.js
+++ fundamentos/destructuring.js
@@ -36,3 +36,15 @@ console.log(n1, n3, n5, n6)
 
 const [, [, nota]] = [[, 8, 8], [9, 6, 8]]
 console.log(nota)
+
+//DESTRUCTURING COM FUNÇÃO
+
+console.log('*****************************')
+function rand({min = 0, max = 1000}){
+    const valor = Math.random() * (max - min) + min 
+    return Math.floor(valor)
+}
+const obj = {max: 50, min: 40}
+console.log(rand(obj)) //considera os parametros de obj
+console.log(rand({min: 955})) // considera o maximo definido na função
+console.log(rand({})) //considera os paramentros de min e max definidos na função
