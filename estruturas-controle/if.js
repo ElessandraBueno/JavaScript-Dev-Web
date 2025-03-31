48946075f602c77d767ad6f55970fd07819e91e3
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-03-30 21:07:48 -0300

    Estrutura de controle - IF

--- estruturas-controle/if.js
+++ estruturas-controle/if.js
@@ -8,7 +8,7 @@ notaEscolar(8.1)
 notaEscolar(5.8)
 
 
-//Só irá imprimir
+//Só irá imprimir se o valor for considerado true
 function soVerdade(valor){
     if(valor){
         console.log('É verdade que...' + valor)
@@ -27,3 +27,32 @@ soVerdade([1, 2]) //true
 soVerdade({})
 soVerdade(true)
 soVerdade(false)
+
+//Se utilizar o IF sem as chaves, só pode haver uma linha de código e as sentenças precisam ser separadas por vírgula
+function teste1(num){
+    if(num > 7)
+        console.log(num) ; console.log('final')
+}
+teste1(6)
+teste1(8)
+
+//Da forma como está o trecho abaixo, a segunda setença não está dentro do IF, por isso ela é executado duas vezes
+function teste2(num1){
+    if(num1 > 7)
+        console.log(num1)
+        console.log('final')
+}
+teste1(6)
+teste1(8)
+
+//O ; antes das chaves faz com que o IF seja uma sentença vazia e o bloco de código entre chaves não tem nenhuma ligação com o IF
+function teste3(num2){
+    if(num2 > 7)
+        ;
+    {
+        console.log(num2)
+    }
+}
+teste3(9)
+teste3(1)
+
