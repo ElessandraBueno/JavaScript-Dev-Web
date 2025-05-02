365fedf900826399934a54dd00a5483ff1e4f207
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-05-02 20:37:19 -0300

    Cidadão de primeira classe em JS - Função

---
+++ funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,40 @@
+//Função em JS é First-class Object (citizens)
+//Higher-Order Function
+
+//Criando de forma literal
+function fun1(){
+
+}
+
+//Armazenar em uma variável
+const fun2 = function (){
+
+}
+
+//Armazenar em um array
+const array = [
+    function (a,b){return a+b}, 
+    fun1, 
+    fun2]
+console.log(array[0](2,3))
+
+//Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function(){ return 'Opa'}
+console.log(obj.falar())
+
+//Função como parametro
+function run(teste){
+    teste()
+}
+run(function(){console.log('Executando...')})
+
+//Uma função pode retornar/conter uma outra função
+function soma(a,b){
+    return function(c){
+        console.log(a+b+c)
+    }
+}
+soma(2,3)(5)
+const teste2 = soma(2,3)
+teste2(4)
