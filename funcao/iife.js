ee0bf78594bfbcba18c1bdb89679dd7fd862c12d
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-05-26 08:14:29 -0300

    Call e Apply

---
+++ funcao/iife.js
@@ -0,0 +1,7 @@
+//IIFE -> Immediately Invoked Function Expression
+
+//Tem que estar dentro de parenteses e ser chamada por parentese em seguida
+(function (){
+    console.log('Será executado na hora!')
+    console.log("Foge do escopo mais abrangente!")
+})()
