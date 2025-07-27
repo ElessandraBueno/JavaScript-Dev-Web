f7f6010eacf5daba8ab36e65ee8d8e22931fe69a
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-07-27 10:14:26 -0300

    Desafios do capitulo de funções

---
+++ funcao/desafios/desafio1.js
@@ -0,0 +1,12 @@
+//01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração, multiplicação e divisão desses valores.
+
+function operacoes(num1, num2) {
+    console.log(`Num1: ${num1} \nNum2: ${num2}`)
+    console.log(`Soma: ${num1 + num2}`)
+    console.log(`Subtração: ${num1 - num2}`)
+    console.log(`Multiplicação: ${num1 * num2}`)
+    console.log(`Divisão: ${num1 /num2}`)
+}
+
+operacoes(6, 2)
+
