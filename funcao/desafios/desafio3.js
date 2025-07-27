898c313ff4dcbbbeee0c388157cb8d8d165c0db3
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-07-27 10:34:38 -0300

    Desafio 3 - expoente

---
+++ funcao/desafios/desafio3.js
@@ -0,0 +1,8 @@
+//03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function expo(base, expo){
+    return Math.pow(base, expo)
+}
+console.log(expo(2, 2))
+console.log(expo(4, 3))
+console.log(expo(10, 2))
