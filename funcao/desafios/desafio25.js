7b0b1fdaf63f9e19475cec544aae4088ff0a3f14
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-07-28 07:50:51 -0300

    Desafio 25 - imprimir até 50

---
+++ funcao/desafios/desafio25.js
@@ -0,0 +1,13 @@
+// 25) Escrever um programa para exibir os números de 1 até 50 na tela.
+
+let i = 1
+
+while(i <= 50){
+    console.log(i)
+    i++
+}
+
+for(let x = 1; x < 51; x++){
+    console.log('for - '+x)
+}
+
