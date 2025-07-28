68120b6a16e4f70673ed59521c822b18fd9ec724
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-07-28 07:47:50 -0300

    Desafio 24 - estrutura while

---
+++ funcao/desafios/desafio24.js
@@ -0,0 +1,9 @@
+// 24) Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+
+let frase = 'Hello Word!'
+let i = 1
+
+while(i < 12){
+    console.log(frase+' - ' + i)
+    i++
+}
