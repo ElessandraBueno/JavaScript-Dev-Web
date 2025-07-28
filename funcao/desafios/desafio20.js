9278f6281a1b9a4c43e936ff1413960eb0d153ec
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-07-28 06:54:23 -0300

    Desafio 20 - saque dinheiro

---
+++ funcao/desafios/desafio20.js
@@ -0,0 +1,82 @@
+// 20) Crie um programa para informar quais e quantas notas são necessárias para entregar o mínimo de cédulas para um determinado valor informado pelo usuário considerando notas de R$ 100, R$ 50, R$ 10 e R$ 5 e R$ 1. Seu programa deve mostrar apenas as notas utilizadas. 
+//Por exemplo, ao solicitar R$18, o programa deve informar apenas a seguinte informação (note que não foram exibidas informações sobre as demais cédulas): 1 nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1.
+
+function sacarDinheiro(valorSaque){
+    let contador100 = 0
+    let contador50 = 0
+    let contador10 = 0
+    let contador5 = 0
+    let contador1 = 0
+
+    //ve qual a maior nota que cabe no valor restante
+    let valorNota = calcularValorNota(valorSaque)
+
+    //loop para descontar notas enquanto for possível
+    while(valorSaque >= valorNota){
+        switch(valorNota){
+            case 100:
+                valorSaque -= 100
+                contador100++
+                break;
+            case 50:
+                valorSaque -= 50
+                contador50++
+                break;
+            case 10:
+                valorSaque -= 10
+                contador10++
+                break;
+            case 5:
+                valorSaque -= 5
+                contador5++
+                break;
+            case 1:
+                valorSaque -= 1
+                contador1++
+                break;
+        }
+
+        //atualiza o valor da nota conforme descontado a cédula anterior
+        valorNota = calcularValorNota(valorSaque)
+    }
+    return elaborarResultado(contador100, contador50, contador10, contador5, contador1)
+}
+
+//retorna a maior nota possível que ainda cabe no valor
+function calcularValorNota(valorSaque){
+    if(valorSaque >= 100){
+        return 100
+    }else if(valorSaque >= 50){
+        return 50
+    }else if(valorSaque >= 10){
+        return 10
+    }else if(valorSaque >= 5){
+        return 5
+    }else if(valorSaque >= 1){
+        return 1
+    }
+}
+
+function elaborarResultado(contador100, contador50, contador10, contador5, contador1){
+    let resultado = ''
+
+    if(contador100 > 0){
+        resultado += `${contador100} nota(s) de R$100.\n`
+    }
+    if(contador50 > 0){
+        resultado += `${contador50} nota(s) de R$50.\n`
+    }
+    if(contador10 > 0){
+        resultado += `${contador10} nota(s) de R$10.\n`
+    }
+    if(contador5 > 0){
+        resultado += `${contador5} nota(s) de R$5.\n`
+    }
+    if(contador1 > 0){
+        resultado += `${contador1} nota(s) de R$1.\n`
+    }
+
+    return resultado
+}
+
+console.log(sacarDinheiro(151))
