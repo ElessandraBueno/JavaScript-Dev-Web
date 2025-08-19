d3a5c2df41a2d0ac7c78db8fc7374f7a0a526b29
Elessandra Bueno <72478092+ElessandraBueno@users.noreply.github.com>
2025-08-19 20:30:33 -0300

    Map

--- array/map1.js
+++ array/map1.js
@@ -1,8 +1,15 @@
 const nums = [1, 2, 3, 4, 5]
-
+console.log(nums)
 //MAP - Faz alterações em cada um dos elementos do array e cria um outro array com as alterações, sem modificar o array original
 let resultado = nums.map(function(e){
     return e * 2
 })
 
+console.log(resultado)
+
+const soma10 = e => e + 10
+const triplo = e => e * 3
+const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.',',')}`
+
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro)
 console.log(resultado)
